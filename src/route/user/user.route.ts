e0fd8ef7efe96904803fef949e4fa362cbf357e5
Ivan <welka2018@mail.ru>
2026-06-22 14:45:43 +0300

    add logout

--- src/route/user/user.route.ts
+++ src/route/user/user.route.ts
@@ -1,15 +1,34 @@
 import {Hono} from "hono";
+import {deleteCookie, getCookie} from 'hono/cookie'
+import type {ResponseResult} from "../../shared/types/response-request.types.js";
 
 const user = new Hono().basePath('/')
 
-user.get('/:id', (c) => {
-    const id = c.req.param('id')
-    return c.text(`User ${id}`, 200)
+user.get('/me', async (c) => {
+    return c.text('Hello!!!')
+})
+
+user.post('/logout', (c) => {
+    const cookie = getCookie(c, 'token');
+
+    if (cookie) deleteCookie(c, 'token');
+    else {
+        return c.json({
+            success: false,
+            error: {
+                details: 'Необходимые данные для выхода не найдены'
+            }
+        } as ResponseResult, 400)
+    }
+
+    return c.json({
+        success: true,
+        data: 'Выход успешно выполнен'
+    } as ResponseResult, 200)
 })
 
 user.post('/', async (c) => {
-    return c.json({
-    }, 201)
+    return c.json({}, 201)
 })
 
 export default user;
